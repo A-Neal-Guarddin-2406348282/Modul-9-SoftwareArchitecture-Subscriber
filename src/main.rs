c8d5666f5dbc3535963a7020cfd74737ced68f74
Neal Guarddin <guarddin29@gmail.com>
2026-05-12 06:43:01 +0700

    Sending and processing event

--- src/main.rs
+++ src/main.rs
@@ -15,7 +15,7 @@ impl MessageHandler<UserCreatedEventMessage> for UserCreatedHandler {
         let ten_millis = time::Duration::from_millis(1000);
         let _now = time::Instant::now();
 
-        thread::sleep(ten_millis);
+        // thread::sleep(ten_millis);
 
         println!("In Neal’s Computer [2406348282]. Message received: {:?}", message);
         Ok(())
